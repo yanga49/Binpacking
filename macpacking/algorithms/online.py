cb6477a2d43c5b6fb6456606383b40d988c0df7b
Akanksha <akanksha@Akankshas-MacBook-Air.local>
2022-10-28 11:06:21 -0400

    formatted bin packing algos

--- macpacking/algorithms/online.py
+++ macpacking/algorithms/online.py
@@ -22,30 +22,21 @@ class NextFit(Online):
 class FirstFit(Online):
     
     def _process(self, capacity: int, stream: WeightStream) -> Solution:
-        res = 0
-        # Create an array to store remaining space in bins
-        # there can be at most n bins
-        bin_rem = []
+        bin_count = 0
+        bin_cap = []
         solution = []
-        
-        # Place items one by one
         for w in stream:
-            bin_rem.append([0])
-        
-            # Find the first bin that can accommodate
-            # w
+            bin_cap.append([0])
             j = 0
-            while( j < res):
-                if (bin_rem[j] >= w):
-                    bin_rem[j] = bin_rem[j] - w
+            while (j < bin_count):
+                if (bin_cap[j] >= w):
+                    bin_cap[j] = bin_cap[j] - w
                     solution[j].append(w)
                     break
                 j+=1
-                
-            # If no bin could accommodate w
-            if (j == res):
-                bin_rem[res] = capacity - w
-                res = res+1
+            if (j == bin_count):
+                bin_cap[bin_count] = capacity - w
+                bin_count = bin_count+1
                 solution.append([w])
         return solution
 
@@ -53,71 +44,55 @@ class FirstFit(Online):
 class BestFit(Online):
 
     def _process(self, capacity: int, stream: WeightStream) -> Solution:
-        res = 0
+        bin_count = 0
     
-        bin_rem = []
+        bin_cap = []
         solution = []
     
         for w in stream:
-            bin_rem.append([0])
+            bin_cap.append([0])
             j = 0
             min = capacity + 1
             bi = 0
     
-            for j in range(res):
-                if (bin_rem[j] >= w and bin_rem[j] - w < min):
+            for j in range(bin_count):
+                if (bin_cap[j] >= w and bin_cap[j] - w < min):
                     bi = j
-                    min = bin_rem[j] - w
+                    min = bin_cap[j] - w
                 
-            # If no bin could accommodate w,
-            # create a new bin
             if (min == capacity + 1):
-                bin_rem[res] = capacity - w
-                res += 1
+                bin_cap[bin_count] = capacity - w
+                bin_count += 1
                 solution.append([w])
-            else: # Assign the item to best bin
-                bin_rem[bi] -= w
+            else: 
+                bin_cap[bi] -= w
                 solution[j].append(w)
         return solution
 
 class WorstFit(Online):
 
     def _process(self, capacity: int, stream: WeightStream) -> Solution:
-        # Initialize result (Count of bins)
-        res = 0
-    
-        # Create an array to store remaining space in bins
-        # there can be at most n bins
-        bin_rem = []
+        bin_count = 0
+        bin_cap = []
         solution = []
-        # Place items one by one
         for w in stream:
-            bin_rem.append([0])
-            # Find the best bin that ca\n accommodate
-            # w
-    
-            # Initialize maximum space left and index
-            # of worst bin
+            bin_cap.append([0])
             mx,wi = -1,0
     
-            for j in range(res):
-                if (bin_rem[j] >= w and bin_rem[j] - w > mx):
+            for j in range(bin_count):
+                if (bin_cap[j] >= w and bin_cap[j] - w > mx):
                     wi = j
-                    mx = bin_rem[j] - w
-                
-    
-            # If no bin could accommodate w,
-            # create a new bin
+                    mx = bin_cap[j] - w
+            
             if (mx == -1):
-                bin_rem[res] = capacity - w
-                res += 1
+                bin_cap[bin_count] = capacity - w
+                bin_count += 1
                 solution.append([w])
             
-            else: # Assign the item to best bin
-                bin_rem[wi] -= w
+            else:
+                bin_cap[wi] -= w
                 solution[j].append(w)
                 
-        
         return solution
 
 
