27e653f46e0f5a8aa002282f7d559f33c09e4ade
Akanksha <akanksha@Akankshas-MacBook-Air.local>
2022-10-28 10:58:59 -0400

    added online and offline bin packing algorithm implementations

--- macpacking/algorithms/offline.py
+++ macpacking/algorithms/offline.py
@@ -1,6 +1,9 @@
 from .. import Solution, WeightSet
 from ..model import Offline
 from .online import NextFit as Nf_online
+from .online import FirstFit as Ff_online
+from .online import WorstFit as Wf_online
+from .online import BestFit as Bf_online
 
 
 class NextFit(Offline):
@@ -10,4 +13,33 @@ class NextFit(Offline):
         delegating to the online version (avoiding code duplication)'''
         weights = sorted(weights, reverse=True)
         delegation = Nf_online()
+        return delegation((capacity, weights))
+
+
+class FirstFitDecreasing(Offline):
+
+    def _process(self, capacity: int, weights: WeightSet) -> Solution:
+        '''An offline version of FirstFit, ordering the weigh stream and
+        delegating to the online version (avoiding code duplication)'''
+        weights = sorted(weights, reverse=True)
+        delegation = Ff_online()
+        return delegation((capacity, weights))
+        
+
+class BestFitDecreasing(Offline):
+
+    def _process(self, capacity: int, weights: WeightSet) -> Solution:
+        '''An offline version of BestFit, ordering the weigh stream and
+        delegating to the online version (avoiding code duplication)'''
+        weights = sorted(weights, reverse=True)
+        delegation = Bf_online()
+        return delegation((capacity, weights))
+
+class WorstFitDecreasing(Offline):
+
+    def _process(self, capacity: int, weights: WeightSet) -> Solution:
+        '''An offline version of WorstFit, ordering the weigh stream and
+        delegating to the online version (avoiding code duplication)'''
+        weights = sorted(weights, reverse=True)
+        delegation = Wf_online()
         return delegation((capacity, weights))
--- macpacking/algorithms/online.py
+++ macpacking/algorithms/online.py
@@ -17,3 +17,107 @@ class NextFit(Online):
                 solution.append([w])
                 remaining = capacity - w
         return solution
+
+
+class FirstFit(Online):
+    
+    def _process(self, capacity: int, stream: WeightStream) -> Solution:
+        res = 0
+        # Create an array to store remaining space in bins
+        # there can be at most n bins
+        bin_rem = []
+        solution = []
+        
+        # Place items one by one
+        for w in stream:
+            bin_rem.append([0])
+        
+            # Find the first bin that can accommodate
+            # w
+            j = 0
+            while( j < res):
+                if (bin_rem[j] >= w):
+                    bin_rem[j] = bin_rem[j] - w
+                    solution[j].append(w)
+                    break
+                j+=1
+                
+            # If no bin could accommodate w
+            if (j == res):
+                bin_rem[res] = capacity - w
+                res = res+1
+                solution.append([w])
+        return solution
+
+
+class BestFit(Online):
+
+    def _process(self, capacity: int, stream: WeightStream) -> Solution:
+        res = 0
+    
+        bin_rem = []
+        solution = []
+    
+        for w in stream:
+            bin_rem.append([0])
+            j = 0
+            min = capacity + 1
+            bi = 0
+    
+            for j in range(res):
+                if (bin_rem[j] >= w and bin_rem[j] - w < min):
+                    bi = j
+                    min = bin_rem[j] - w
+                
+            # If no bin could accommodate w,
+            # create a new bin
+            if (min == capacity + 1):
+                bin_rem[res] = capacity - w
+                res += 1
+                solution.append([w])
+            else: # Assign the item to best bin
+                bin_rem[bi] -= w
+                solution[j].append(w)
+        return solution
+
+class WorstFit(Online):
+
+    def _process(self, capacity: int, stream: WeightStream) -> Solution:
+        # Initialize result (Count of bins)
+        res = 0
+    
+        # Create an array to store remaining space in bins
+        # there can be at most n bins
+        bin_rem = []
+        solution = []
+        # Place items one by one
+        for w in stream:
+            bin_rem.append([0])
+            # Find the best bin that ca\n accommodate
+            # w
+    
+            # Initialize maximum space left and index
+            # of worst bin
+            mx,wi = -1,0
+    
+            for j in range(res):
+                if (bin_rem[j] >= w and bin_rem[j] - w > mx):
+                    wi = j
+                    mx = bin_rem[j] - w
+                
+    
+            # If no bin could accommodate w,
+            # create a new bin
+            if (mx == -1):
+                bin_rem[res] = capacity - w
+                res += 1
+                solution.append([w])
+            
+            else: # Assign the item to best bin
+                bin_rem[wi] -= w
+                solution[j].append(w)
+                
+        
+        return solution
+
+
