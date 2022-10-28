212925198be879f19541d0311944df975d6522fd
Akanksha <akanksha@Akankshas-MacBook-Air.local>
2022-10-28 12:53:13 -0400

    integrated baseline using to_constant_bin_number() method

--- macpacking/algorithms/baseline.py
+++ macpacking/algorithms/baseline.py
@@ -7,3 +7,12 @@ class BenMaier(Offline):
 
     def _process(self, capacity: int, weights: list[int]) -> Solution:
         return bp.to_constant_volume(weights, capacity)
+
+class ConstantBinNumber(Offline):
+
+    def __init__(self, num_bins) -> None:
+        super().__init__()
+        self.num_bins = num_bins
+
+    def _process(self, capacity: int, weights: list[int]) -> Solution:
+        return bp.to_constant_bin_number(weights, self.num_bins)
